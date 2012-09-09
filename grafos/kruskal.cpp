7df069a92d21da5c39f38439a05a6ed55bb08e49
Luiza Silva <luiza@royalsflush.net>
2012-09-09 15:36:16 -0300

    adicionei um argumento de template pro kruskal

--- grafos/kruskal.cpp
+++ grafos/kruskal.cpp
@@ -1,22 +1,22 @@
 //Kruskal
 //Testado em:
 //Live Archive 4296
-//	  
+//UVa 10034	  
 
-#include <stdio.h>
 #include <algorithm>
 #include <string.h>
-#include <vector>
 using namespace std;
 
 #define MAXV 3010
 #define MAXE 100010
 
+template <typename T>
+
 struct edge {
 	int a,b;
-	int w;
+	T w;
 
-	edge(int pa=0, int pb=0, int pw=0) :
+	edge(int pa=0, int pb=0, T pw=0) :
 		a(pa), b(pb), w(pw) {}
 	bool operator<(const edge& e) const {
 		return this->w<e.w;
@@ -25,7 +25,7 @@ struct edge {
 
 int rank[MAXV];
 int rep[MAXV];
-edge ls[MAXE];
+edge<double> ls[MAXE];
 int n,m;
 
 int find(int a) {
@@ -48,15 +48,16 @@ bool unio(int a, int b) {
 	return true;
 }
 
-void kruskal() {
+double kruskal() {
 	memset(rank,0,sizeof(rank));
 	sort(ls,ls+m);	
 
 	for (int i=0; i<n; i++)
 		rep[i]=i;
 
+	double sum=0;
 	for (int i=0; i<m; i++)
-		if (unio(ls[i].a, ls[i].b)) {
-		}
+		if (unio(ls[i].a, ls[i].b)) 
+			sum+=ls[i].w;
+	return sum;
 }
-
